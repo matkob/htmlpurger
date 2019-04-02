e819f1b9b127147088fbe9d07c4ef5b9d523ac91
Mateusz <nologin90@gmail.com>
2019-04-02 11:20:42 +0200

    html lexer init

--- src/main/java/com/mkobiers/htmlpurger/lexer/HtmlLexer.java
+++ src/main/java/com/mkobiers/htmlpurger/lexer/HtmlLexer.java
@@ -2,6 +2,7 @@ package com.mkobiers.htmlpurger.lexer;
 
 import com.mkobiers.htmlpurger.io.IReader;
 import com.mkobiers.htmlpurger.model.GrammarException;
+import com.mkobiers.htmlpurger.model.Token;
 import com.mkobiers.htmlpurger.model.TokenType;
 
 import static com.mkobiers.htmlpurger.model.TokenType.*;
@@ -18,33 +19,19 @@ public class HtmlLexer {
         this.builder = new StringBuilder();
     }
 
-    public void nextToken() throws GrammarException {
+    public Token nextToken() throws GrammarException {
         char c;
         String text;
         switch (current) {
             case TAG:
-                current = OPENTAG;
-                break;
             case OPENTAG:
-                current = TAGOPEN_LEFT;
-            case CONTENT:
-            case CLOSETAG:
-            case NAME:
-                while ((c = reader.nextChar()) != ' ' && c != '>') {
-                    builder.append(c);
-                }
-                if (c == ' ') {
-                    current = ATTR_NAME;
-                } else {
-                    current = CLOSETAG;
-                }
-                text = builder.toString();
-                builder = new StringBuilder();
-                //return new Token(text, NAME);
+            case CONTENT: return buildContent();
 
+            case CLOSETAG: return buildClosetag();
+            case TAGOPEN_NAME: return buildTagopenName();
             case ATTRIBUTES:
             case ATTR:
-            case ATTR_NAME:
+            case ATTR_NAME: return buildAttrName();
             case EQUALS:
             case VALUE:
             case STRING:
@@ -54,21 +41,102 @@ public class HtmlLexer {
             case DOUBLEQUOTE:
             case NUM:
             case TEXT:
-            case TAGOPEN_LEFT:
-                while ((c = reader.nextChar()) == ' ' || c == '\n' || c == '\t');
-                if (c == '<') {
-                    current = NAME;
-                    //return new Token("<", TAGOPEN_LEFT);
-                }
-                throw new GrammarException();
-
+            case TAGOPEN_LEFT: return buildTagopenLeft();
+            case TAGOPEN_RIGHT: return buildTagopenRight();
             case TAGCLOSE_LEFT:
-            case TAG_RIGHT:
+            case TAGCLOSE_RIGHT:
             case END_OF_TEXT:
 
-
-
         }
+        return null;
+    }
+
+    private Token buildContent() throws GrammarException {
+        char c;
+        c = reader.nextChar();
+        builder.append(c);
+        if (c == '<') {
+            current = TAGOPEN_LEFT;
+            return buildTagopenLeft();
+        } else {
+            current = TEXT;
+            return buildText();
+        }
+    }
+
+    private Token buildTagopenLeft() throws GrammarException {
+        char c;
+        c = truncate();
+        if (c == '<') {
+            current = TAGOPEN_NAME;
+            return new Token("<", TAGOPEN_LEFT);
+        }
+        throw new GrammarException();
+    }
+
+    private Token buildTagopenRight() throws GrammarException {
+        char c;
+        c = truncate();
+        if (c == '>') {
+            current = CONTENT;
+            return new Token(">", TAGOPEN_RIGHT);
+        }
+        throw new GrammarException();
+    }
+
+    private Token buildTagopenName() throws GrammarException {
+        char c;
+        String text;
+        while ((c = reader.nextChar()) != ' ' && c != '>') {
+            builder.append(c);
+        }
+        if (c == ' ') {
+            current = ATTR_NAME;
+        } else {
+            current = TAGOPEN_RIGHT;
+        }
+        text = builder.toString();
+        builder = new StringBuilder();
+        return new Token(text, TAGOPEN_NAME);
+    }
+
+    private Token buildClosetag() throws GrammarException {
+        char c;
+        String text;
+        return null;
+    }
+
+    private Token buildAttrName() throws GrammarException {
+        char c;
+        String text;
+        while ((c = reader.nextChar()) != '=' && c != '>') {
+            builder.append(c);
+        }
+        if (c == '=') {
+            current = VALUE;
+        } else {
+            current = CONTENT;
+        }
+        text = builder.toString();
+        builder = new StringBuilder();
+        return new Token(text, ATTR_NAME);
+    }
+
+    private Token buildText() throws GrammarException {
+        char c;
+        String text;
+        while ((c = reader.nextChar()) != '<') {
+            builder.append(c);
+        }
+        current = CLOSETAG;
+        text = builder.toString();
+        return new Token(text, TEXT);
+    }
+
+    private char truncate() {
+        char c;
+        while ((c = reader.nextChar()) == ' ' || c == '\n' || c == '\t');
+        return c;
     }
 
 }
