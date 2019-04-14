d32d0567adc046a4b655626fe9e87058c09639de
Mateusz <nologin90@gmail.com>
2019-04-14 11:07:41 +0200

    help option

--- src/main/java/com/mkobiers/htmlpurger/HtmlPurgerApp.java
+++ src/main/java/com/mkobiers/htmlpurger/HtmlPurgerApp.java
@@ -12,6 +12,7 @@ public class HtmlPurgerApp {
         Options options = new Options();
         options.addOption("p", "page", true, "source of html file");
         options.addOption("c", "conf", true, "source of config file");
+        options.addOption("h", "help", false, "displays this message");
 
         HtmlFilter filter = new HtmlFilter();
 
@@ -19,12 +20,19 @@ public class HtmlPurgerApp {
             CommandLineParser parser = new DefaultParser();
             CommandLine cmd = parser.parse(options, args);
 
-            if(cmd.hasOption("p") && cmd.hasOption("c")) {
+            if (cmd.hasOption("h")){
+                String header = "HTMLpurger lets you clean your html\n\n";
+                String footer = "\nmkobiers solutions inc 2019";
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("htmlpurger", header, options, footer, true);
+                return;
+            }
+
+            if (cmd.hasOption("p") && cmd.hasOption("c")) {
                 String htmlPath = cmd.getOptionValue("p");
                 String configPath = cmd.getOptionValue("c");
                 logger.info("Using html file " + htmlPath);
                 logger.info("Using config file " + configPath);
-
 
             } else {
                 logger.error("No specified filepaths");
